e522a190b89418543ede22e92e20888657ed7583
Sheel Sindhu Prabhakar <sheel.prabhakar@gmail.com>
2021-01-26 12:10:47 +0530

    Initial code

---
+++ spring-boot/java/src/main/java/org/c4c/countrystatecity/CountrystatecityApplication.java
@@ -0,0 +1,14 @@
+package org.c4c.countrystatecity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountrystatecityApplication {
+
+	public static void main(final String[] args) {
+		SpringApplication.run(CountrystatecityApplication.class, args);
+	}
+
+}
+
---
+++ spring-boot/java/src/test/java/org/c4c/countrystatecity/CountrystatecityApplicationTests.java
@@ -0,0 +1,13 @@
+package org.c4c.countrystatecity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountrystatecityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
